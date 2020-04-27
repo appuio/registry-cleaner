da9faef478b8175da2ba88ef34ea882699f0732b
Manuel Hutter <manuel@vshn.ch>
2020-04-27 11:01:47 +0200

    Divide functionality into subcommands
    Signed-off-by: Manuel Hutter <manuel@vshn.ch>

--- cmd/arc/main.go
+++ cmd/arc/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"errors"
+	"fmt"
 	"log"
 	"os"
 	"path/filepath"
@@ -8,35 +10,74 @@ import (
 	"github.com/appuio/arc"
 )
 
+const usage = `usage: arc [command]
+
+commands:
+  uploads: Delete repos that have uploads but no manifests or layers
+  repos:   Delete repositories that are not known to OpenShift
+  blobs:   Delete orphaned blobs
+`
+
 func main() {
-	if err := run(); err != nil {
+	if err := run(os.Args[1:]); err != nil {
 		log.Fatalln(err)
 	}
 }
 
-func run() error {
+func run(args []string) error {
+	if len(args) != 1 {
+		fmt.Print(usage)
+		os.Exit(1)
+	}
+
 	osc := getOpenShiftClient()
 	s3c := getS3Client()
 	reg := getRegistryClient()
 
-	osImages := must(osc.ImageMap()).(map[string]string)
-	log.Printf("Found %d Images in OpenShift", len(osImages))
+	switch args[0] {
+	case "uploads":
+		return cleanupInvalidUploads(s3c, reg)
 
-	repos, err := s3c.ReadRepos()
-	if err != nil {
-		return err
+	case "repos":
+		return cleanupOrphanedRepos(s3c, reg, osc)
+
+	case "blobs":
+		return cleanupOrphanedBlobs(s3c)
+
+	default:
+		log.Println("Invalid command", args[0])
+		fmt.Print(usage)
+		os.Exit(1)
 	}
+
+	return nil
+}
+
+func cleanupInvalidUploads(s3c *arc.S3Client, reg *arc.RegistryClient) error {
+	repos := must(s3c.ReadRepos()).(map[string]*arc.Repository)
 	log.Printf("Got %d repos from S3", len(repos))
 
 	for name, r := range repos {
 		if !r.HasLayers() && !r.HasManifests() {
 			log.Println(name, "has neither Layers nor Manifests, deleting")
 			if err := reg.DeleteRepo(name); err != nil {
-				log.Fatalln("Error deleting Repo:", err)
+				return fmt.Errorf("Error deleting repo: %w", err)
 			}
 			continue
 		}
+	}
 
+	return nil
+}
+
+func cleanupOrphanedRepos(s3c *arc.S3Client, reg *arc.RegistryClient, osc *arc.OpenShiftClient) error {
+	osImages := must(osc.ImageMap()).(map[string]string)
+	log.Printf("Found %d Images in OpenShift", len(osImages))
+
+	repos := must(s3c.ReadRepos()).(map[string]*arc.Repository)
+	log.Printf("Got %d repos from S3", len(repos))
+
+	for name, r := range repos {
 		hasMatch := false
 		for _, digest := range r.ManifestRevisions {
 			if osImages[digest] != "" {
@@ -52,11 +93,15 @@ func run() error {
 
 		log.Printf("Found no manifest revision for '%s' in OpenShift, deleting!", name)
 		if err := reg.DeleteRepo(name); err != nil {
-			log.Fatalln("Error deleting Repo:", err)
+			return fmt.Errorf("Error deleting Repo: %w", err)
 		}
 	}
 
 	return nil
+}
+
+func cleanupOrphanedBlobs(s3c *arc.S3Client) error {
+	return errors.New("Not yet implemented")
 }
 
 func must(v interface{}, err error) interface{} {
